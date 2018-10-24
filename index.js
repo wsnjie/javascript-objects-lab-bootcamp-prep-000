ccca01c021070c030e35ef397ffcb086a771e299
William Njie <wsnjie@gmail.com>
2018-10-24 02:39:08 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -7,5 +7,7 @@ function destructivelyUpdateObjectWithKeyAndValue(object, key, value){
   return object
 }
 function deleteFromObjectByKey(object, key){
-  delete object[key]
+var newObj = Object.assign({}, object)
+  delete newObj[key]
+  return newObj
 }
