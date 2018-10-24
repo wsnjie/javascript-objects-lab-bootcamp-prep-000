7a2ce13f04eb06ea1b66dd9bc5c2d4fbb5dfc71d
William Njie <wsnjie@gmail.com>
2018-10-24 02:33:54 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -2,3 +2,6 @@ var recipes = new Object()
 function updateObjectWithKeyAndValue(object, key, value){
 return Object.assign({}, object, { [key]: value })
 }
+funcion destructivelyUpdateObjectWithKeyAndValue(object, key, value){
+  return object[key] = value
+}
